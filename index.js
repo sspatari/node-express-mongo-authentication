1d858cd9380f62d345fb26f0dd1e6e6bb0afaf09
sspatari <spatari.stanislav@gmail.com>
2021-03-09 01:08:39 +0200

    feat(server): create simple server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
