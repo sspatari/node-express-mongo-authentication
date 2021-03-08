810a243be04fe5be58906116e7d10b8add20bacd
sspatari <spatari.stanislav@gmail.com>
2021-03-09 01:33:27 +0200

    feat(endpoint): add root endpoint and inexistent route handlers

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/', (_req, res, _next) => {
+    res.send('Hello from express');
+});
+
+app.use((_res, _req, next) => {
+    next(createError.NotFound());
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        },
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
