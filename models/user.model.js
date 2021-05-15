75528a04f6eb3e7f20e2d2f41e686f916098f689
sspatari <spatari.stanislav@gmail.com>
2021-05-16 02:33:53 +0300

    feat(register-route): encrypt password before saving

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     email: {
         type: String,
         required: true,
@@ -14,6 +15,17 @@ const userSchema = new Schema({
     },
 });
 
-const User = mongoose.model('user', userSchema);
+UserSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
