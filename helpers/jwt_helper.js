7dc2bb3e37e05c6e96db64800d06de6a3c63cec9
sspatari <spatari.stanislav@gmail.com>
2021-05-16 04:13:30 +0300

    feat: use a env secrets for accessToken and refreshToken generation

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -5,7 +5,7 @@ module.exports = {
     signAccessToken: (userId) => {
         return new Promise((resolve, reject) => {
             const payload = {};
-            const secret = 'some super secret';
+            const secret = process.env.ACCESS_TOKEN_SECRET;
             const options = {
                 expiresIn: '1h',
                 issuer: 'pickurpage.com',
