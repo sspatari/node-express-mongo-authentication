6c136ffe707229409cdd45793a488fdccb8fc7d5
sspatari <spatari.stanislav@gmail.com>
2021-05-17 00:52:11 +0300

    feat(helpers): create verifyAccessToken middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const createError = require('http-errors');
 require('dotenv').config();
 require('./helpers/init_mongodb');
+const { verifyAccessToken } = require('./helpers/jwt_helper');
 
 const authRoute = require('./routes/auth.route');
 
@@ -11,7 +12,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (_req, res, _next) => {
+app.get('/', verifyAccessToken, (req, res, _next) => {
     res.send('Hello from express');
 });
 
