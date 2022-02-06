e6fb8f86c9cbbf54d0a329724393a46972a08f3c
Stanislav Spatari <spatari.stanislav@gmail.com>
2022-02-06 16:53:58 +0200

    chore: fix identation in some files

--- models/user.model.js
+++ models/user.model.js
@@ -3,35 +3,35 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 UserSchema.pre('save', async function (next) {
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-        this.password = hashedPassword;
-        next();
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 UserSchema.methods.isValidPassword = async function (password) {
-    try {
-        return await bcrypt.compare(password, this.password);
-    } catch (error) {
-        throw error;
-    }
+  try {
+    return bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
 };
 
 const User = mongoose.model('user', UserSchema);
